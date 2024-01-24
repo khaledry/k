aba63f5658c386881f8f4c2893ac2b68845b59e7
khaledry <157552031+khaledry@users.noreply.github.com>
2024-01-24 05:39:57 +0200

    Create k0.js

---
+++ k0.js
@@ -0,0 +1,88 @@
+var _0x218c = ['.nav-tabs', 'insertBefore', `<form name="jump">
+\
+<div class="MyColors">
+\
+					<map id="ColorsMap" name="ColorsMap">
+\
+					<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle('none',60)" shape="rect" title="1">
+\
+					<area alt="" coords="244, 0, 266, 25" href="javascript:chooseStyle('Co2',60)" shape="rect" title="2">
+\
+					<area alt="" coords="212, 0, 233, 25" href="javascript:chooseStyle('Co3',60)" shape="rect" title="3">
+\
+					<area alt="" coords="179, 0, 201, 25" href="javascript:chooseStyle('Co4',60)" shape="rect" title="4">
+\
+					<area alt="" coords="148, 0, 170, 25" href="javascript:chooseStyle('Co5',60)" shape="rect" title="5">
+\
+					</map>
+\
+			        <img alt="" height="40" src="https://k.top4top.io/p_2310a8ovj1.png" width="300" usemap="#ColorsMap"></div>
+\
+</form>
+\
+
+\
+
+\
+
+\
+
+\
+
+\
+`, 'https://raw.githack.com/khaledry/k/main/color1.js', 'getScript', 'body', `<link rel="stylesheet" type="text/css" href="">
+\
+<link href="https://raw.githack.com/khaledry/k/main/k1.css" rel="stylesheet" type="text/css" />
+\
+<link href="https://raw.githack.com/khaledry/k/main/k2.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />
+\
+<link href="https://raw.githack.com/khaledry/k/main/k3.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" />
+\
+<link href="https://raw.githack.com/khaledry/k/main/k4.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" />
+\
+<link href="https://raw.githack.com/khaledry/k/main/k5.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css" />
+\
+<style>
+\
+
+\
+
+\
+
+\
+.MyColors {
+\
+    position: absolute;
+\
+    transform: matrix(1, 0, 0, 1, 85, -173);
+\
+    margin-top: 2px;
+\
+    width: 300px;
+\
+    height: 60px;
+\
+    float: left;
+\
+    background: transparent url(https://i.ibb.co/dm6jWSL/color1s.png) no-repeat scroll right top;
+\
+    text-align: left;
+\
+}
+\
+
+\
+
+\
+
+\
+
+\
+
+\
+
+\
+
+\
+</style>`];
+$(`${''}${_0x218c[2]}${''}`)[_0x218c[1]](_0x218c[0]), $[_0x218c[4]](_0x218c[3]), $(`${''}${_0x218c[6]}${''}`)[_0x218c[1]](_0x218c[5])

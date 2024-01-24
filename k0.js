a3aa40b9703b1205a0303c8be6e05fe65204626c
khaledry <157552031+khaledry@users.noreply.github.com>
2024-01-24 08:21:13 +0200

    Update k0.js

--- k0.js
+++ k0.js
@@ -1,4 +1,14 @@
-var _0x218c = ['.nav-tabs', 'insertBefore', `<form name="jump">
+var _0xfd94 = ['.nav-tabs', 'insertBefore', `<div id="des333">
+\
+<a id="des444"type="button" style="border-radius: 70px 70px 0px 0px;transform: matrix(1, 0, 0, 1, -19, 2); width: 51%; margin: 2px;" class="btn-des"> .. </a>
+\
+</div>
+\
+
+\
+
+\
+`, `<form name="jump">
 \
 <div class="MyColors">
 \
@@ -14,23 +24,20 @@ var _0x218c = ['.nav-tabs', 'insertBefore', `<form name="jump">
 \
 					<area alt="" coords="148, 0, 170, 25" href="javascript:chooseStyle('Co5',60)" shape="rect" title="5">
 \
+					<area alt="" coords="115, 0, 140, 25" href="javascript:chooseStyle('Co6',60)" shape="rect" title="6">
+\
+					<area alt="" coords="105, 0, 85, 25" href="javascript:chooseStyle('Co7',60)" shape="rect" title="7">
+\
+					<area alt="" coords="75, 0, 52, 25" href="javascript:chooseStyle('Co8',60)" shape="rect" title="8">
+\
 					</map>
 \
-			        <img alt="" height="40" src="" width="300" usemap="#ColorsMap"></div>
+			        <img alt="" height="40" src="https://1.top4top.net/p_1388gqx1c1.gif" width="300" usemap="#ColorsMap"></div>
 \
 </form>
 \
-
 \
-
-\
-
-\
-
-\
-
-\
-`, 'https://raw.githack.com/khaledry/k/main/color1.js', 'getScript', 'body', `<link rel="stylesheet" type="text/css" href="">
+`, 'https://raw.githack.com/khaledry/k/main/color1.js', 'getScript', '.loginstat', 'insertAfter', "<div class='fr borderg' style='padding:2px;background-color:white;margin-left:4px;'> استايلات و ارشفة :<a href='https://www.1des.net/'>ون ديزاين</a></div>", '1px 0', '2px solid #434343', '1px solid #4F565E', '1px solid #434343', '2px', '3px', 'css', '.hand.nosel.fl.uzr.border', '<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">', 'html', '.checkbox label', 'body', `<link rel="stylesheet" type="text/css" href="">
 \
 <link href="https://raw.githack.com/khaledry/k/main/k1.css" media="screen" rel="alternate stylesheet" title="Co1" type="text/css" />
 \
@@ -84,5 +91,526 @@ var _0x218c = ['.nav-tabs', 'insertBefore', `<form name="jump">
 \
 
 \
+<style>
+\
+
+\
+
+\
+
+\
+.MyColors {
+\
+    position: absolute;
+\
+    transform:matrix(1, 0, 0, 1, 75, -2);
+\
+	margin-top: 2px;
+\
+    width: 300px;
+\
+    height: 40px;
+\
+    float: left;
+\
+    background: transparent url(https://i.ibb.co/MkRrdzk/colors.png) no-repeat scroll right top;
+\
+    text-align: left;
+\
+}
+\
+
+\
+
+\
+
+\
+label.label.label-primary.mini.fl {
+\
+    height: 20px;
+\
+    border-radius: 70px 70px 0px 0px!important;
+\
+    padding: 1px;
+\
+	margin-bottom: 1px!important;
+\
+}
+\
+
+\
+
+\
+
+\
+a.label.label-primary.fl {
+\
+    width: 60%!important;
+\
+}
+\
+ button.btn.fr.btn-success.fa.fa-refresh {
+\
+    display: none
+\
+}
+\
+
+\
+a.label.label-primary.fr {
+\
+    height: 25px;
+\
+    text-align: right!important;
+\
+    border-radius: 70px;
+\
+}
+\
+
+\
+.fr.borderg, .fr.borderg a {
+\
+    font-size: 10px!important;
+\
+    padding: 0 5px!important;
+\
+    position: relative;
+\
+    color: #fff;
+\
+	font-family: jazeera,FontAwesome;
+\
+}
+\
+
+\
+.fr.borderg {
+\
+    border: 0px;
+\
+    transform: matrix(1, 0, 0, 1, -50, -130);
+\
+    height: 0;
+\
+}
+\
+
+\
+.minix.noselectvy.fr {
+\
+    width: 376px;
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar {
+\
+    width: 0px;
+\
+    height: 10px
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar-button {
+\
+    width: 5px;
+\
+    height: 5px
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar-corner {
+\
+    background: 0 0
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar-thumb {
+\
+    background: #434343;
+\
+    border: 71px #fff;
+\
+    border-radius: 31px;
+\
+    background-image: -webkit-linear-gradient( 45deg,rgba(255,255,255,.2) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent)!important
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar-track {
+\
+    background: #fff;
+\
+    border: 65px #fff;
+\
+    border-radius: 34px
+\
+}
+\
+
+\
+#des0 {
+\
+    border: 0px dotted#f0ad4e;
+\
+    text-align: center;
+\
+    border-bottom: 0 solid#fff!important;
+\
+    border-right: .5px solid#fff!important;
+\
+    border-top: .5px solid#fff!important;
+\
+    border-left: .5px solid#fff!important;
+\
+    height: 20px;
+\
+    color: #fff;
+\
+    margin-bottom: -3px;
+\
+    font-family: jazeera,FontAwesome;
+\
+    font-size: 13px!important;
+\
+    margin-right: -1px;
+\
+    line-height: 10px;
+\
+    border-radius: 0 0 30px 30px;
+\
+    width: 100%;
+\
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12),0 -3px 1px -2px rgba(0,0,0,.2)!important;
+\
+}
+\
+
+\
+
+\
+.des2 {
+\
+    background-color: #fff;
+\
+    border-radius: 0;
+\
+    margin: 3px 8px!important;
+\
+	border-radius: 15px!important;
+\
+    border-style: solid;
+\
+    border-right: 7px solid;
+\
+    border-left: 7px solid;
+\
+    border-color: #fcf8e3;
+\
+    border-left-style: double;
+\
+    border-right-style: double;
+\
+    font-family: jazeera,FontAwesome;
+\
+    font-size: 13px!important
+\
+}
+\
+
+\
+
+\
+label.label.tc.border.btn.label-info.fl {
+\
+    margin-bottom: 0px!important;
+\
+}
+\
+
+\
+label.addGruMsg.label.tc.border.btn.label-info.fl {
+\
+    margin-top: 0px!important;
+\
+}
+\
+
+\
 </style>`];
-$(`${''}${_0x218c[2]}${''}`)[_0x218c[1]](_0x218c[0]), $[_0x218c[4]](_0x218c[3]), $(`${''}${_0x218c[6]}${''}`)[_0x218c[1]](_0x218c[5])
+<style>
+\
+
+\
+
+\
+
+\
+.MyColors {
+\
+    position: absolute;
+\
+    transform:matrix(1, 0, 0, 1, 75, -2);
+\
+	margin-top: 2px;
+\
+    width: 300px;
+\
+    height: 40px;
+\
+    float: left;
+\
+    background: transparent url(https://i.ibb.co/MkRrdzk/colors.png) no-repeat scroll right top;
+\
+    text-align: left;
+\
+}
+\
+
+\
+
+\
+
+\
+label.label.label-primary.mini.fl {
+\
+    height: 20px;
+\
+    border-radius: 70px 70px 0px 0px!important;
+\
+    padding: 1px;
+\
+	margin-bottom: 1px!important;
+\
+}
+\
+
+\
+
+\
+
+\
+a.label.label-primary.fl {
+\
+    width: 60%!important;
+\
+}
+\
+ button.btn.fr.btn-success.fa.fa-refresh {
+\
+    display: none
+\
+}
+\
+
+\
+a.label.label-primary.fr {
+\
+    height: 25px;
+\
+    text-align: right!important;
+\
+    border-radius: 70px;
+\
+}
+\
+
+\
+.fr.borderg, .fr.borderg a {
+\
+    font-size: 10px!important;
+\
+    padding: 0 5px!important;
+\
+    position: relative;
+\
+    color: #fff;
+\
+	font-family: jazeera,FontAwesome;
+\
+}
+\
+
+\
+.fr.borderg {
+\
+    border: 0px;
+\
+    transform: matrix(1, 0, 0, 1, -50, -130);
+\
+    height: 0;
+\
+}
+\
+
+\
+.minix.noselectvy.fr {
+\
+    width: 376px;
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar {
+\
+    width: 0px;
+\
+    height: 10px
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar-button {
+\
+    width: 5px;
+\
+    height: 5px
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar-corner {
+\
+    background: 0 0
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar-thumb {
+\
+    background: #434343;
+\
+    border: 71px #fff;
+\
+    border-radius: 31px;
+\
+    background-image: -webkit-linear-gradient( 45deg,rgba(255,255,255,.2) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent)!important
+\
+}
+\
+
+\
+div#tlogins ::-webkit-scrollbar-track {
+\
+    background: #fff;
+\
+    border: 65px #fff;
+\
+    border-radius: 34px
+\
+}
+\
+
+\
+#des0 {
+\
+    border: 0px dotted#f0ad4e;
+\
+    text-align: center;
+\
+    border-bottom: 0 solid#fff!important;
+\
+    border-right: .5px solid#fff!important;
+\
+    border-top: .5px solid#fff!important;
+\
+    border-left: .5px solid#fff!important;
+\
+    height: 20px;
+\
+    color: #fff;
+\
+    margin-bottom: -3px;
+\
+    font-family: jazeera,FontAwesome;
+\
+    font-size: 13px!important;
+\
+    margin-right: -1px;
+\
+    line-height: 10px;
+\
+    border-radius: 0 0 30px 30px;
+\
+    width: 100%;
+\
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12),0 -3px 1px -2px rgba(0,0,0,.2)!important;
+\
+}
+\
+
+\
+
+\
+.des2 {
+\
+    background-color: #fff;
+\
+    border-radius: 0;
+\
+    margin: 3px 8px!important;
+\
+	border-radius: 15px!important;
+\
+    border-style: solid;
+\
+    border-right: 7px solid;
+\
+    border-left: 7px solid;
+\
+    border-color: #fcf8e3;
+\
+    border-left-style: double;
+\
+    border-right-style: double;
+\
+    font-family: jazeera,FontAwesome;
+\
+    font-size: 13px!important
+\
+}
+\
+
+\
+
+\
+label.label.tc.border.btn.label-info.fl {
+\
+    margin-bottom: 0px!important;
+\
+}
+\
+
+\
+label.addGruMsg.label.tc.border.btn.label-info.fl {
+\
+    margin-top: 0px!important;
+\
+}
+\
+
+\
+</style>`];
+$(`${''}${_0xfd94[2]}${''}`)[_0xfd94[1]](_0xfd94[0]), $(`${''}${_0xfd94[3]}${''}`)[_0xfd94[1]](_0xfd94[0]), $[_0xfd94[5]](_0xfd94[4]), $(_0xfd94[8])[_0xfd94[7]]($(_0xfd94[6])), $(_0xfd94[16])[_0xfd94[15]]({
+    margin: _0xfd94[9],
+    borderRight: _0xfd94[10],
+    borderLeft: _0xfd94[10],
+    borderTop: _0xfd94[11],
+    borderBottom: _0xfd94[12],
+    paddingTop: _0xfd94[13],
+    borderRadius: _0xfd94[14]
+}), $(_0xfd94[19])[_0xfd94[18]](`${''}${_0xfd94[17]}${''}`), $(`${''}${_0xfd94[21]}${''}`)[_0xfd94[1]](_0xfd94[20])
